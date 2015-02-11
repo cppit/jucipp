8b1bbdd1f9fe1822df03fd5da582d0842ae88617
forgie <tedjohan@gmail.com>
2015-02-11 09:33:16 +0100

    BAB-22 #comment getting the module to communicate #time 8h

--- juci/api.h
+++ juci/api.h
@@ -4,50 +4,74 @@
 //
 //    Plugin API
 //
-namespace juci_plugin{
+namespace juci_api{
   //
   //   calls from python to C++
   //
   namespace cpp{
 
     //
-    //    ReplaceWord:
+    //    Replaceword:
     //        replaces a word in the editor with a string
     //
-    std::string ReplaceWord(const std::string word_);
-
+    void ReplaceWord(const std::string word_){
+      //TODO implement api::ReplaceWord / change string to iter?
+      //some_namespace::controller::replaceWord(word_*);
+      std::cout << "unimplemented function: 'api::ReplaceWord()' called"
+		<< std::endl;
+    }
+        
     //
     //    ReplaceLine:
     //        Replaces a line in the editor with a string
     //
-    std::string ReplaceLine(const std::string line_);
-
-    //
-    //     The module
-    //
-    namespace bp = boost::python;
-    BOOST_PYTHON_MODULE(juci) {
-      // text editing
-      bp::def("replaceLine", &juci_plugin::cpp::ReplaceLine);
-      bp::def("replaceWord", &juci_plugin::cpp::ReplaceWord);
-    }// module::juci
+    void ReplaceLine(const std::string line_){
+      //TODO implement api::ReplaceLine / change string to iter?
+      //some_namespace::controller::replaceLine(line_);
+      std::cout << "unimplemented function: 'api::ReplaceLine()' called"
+		<< std::endl;
+    }    
     
-  }//namespace #include "api.h"
+  }//namespace cpp
 
   //
   //   calls from C++ to Python
   //
   namespace py{
+    const std::string g_project_root("~/bachelor/app/juci/");
+    //helpers
+    boost::python::api::object openPythonScript(const std::string path,
+						boost::python::api::object python_name_space) {
+      std::string temp = g_project_root + path + ".py";
+      boost::python::str the_path(temp);
+      return boost::python::exec_file(the_path, python_name_space);//, python_name_space);
+    }
+    
+    //void snippet(std::string& word);
 
+    void plugin(const std::string& plugin_name){
+      try{
+	/* initialize python interpreter */
+	Py_Initialize();
+	boost::python::api::object main_module = boost::python::import("__main__");
+	boost::python::api::object main_namespace = main_module.attr("__dict__");
+    
+	/* runs script from python */
+	//boost::python::object ignored1 = setPythonVar("word", word, main_namespace);
+	boost::python::api::object ignored2 = openPythonScript(plugin_name, main_namespace);   
+	/* extracts desired values */   
+	//boost::python::object pySnippet =  boost::python::eval("getSnippet()", main_namespace);
+	//word = boost::python::extract<std::string>(pySnippet);
+	/* add snippet to textView */
+	//TODO add snippet
+      }catch(boost::python::error_already_set const&){
+	PyErr_Print();
+      }
+    }
     
   }// py 
   
-}//juci_plugin
+}//juci_api
 
 
-int main(int argc, char *argv[])
-{
-  
-  return 0;
-}
-
+   
