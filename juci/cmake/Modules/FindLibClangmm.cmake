80ecca9d24476f3caa9bd04887aea5ef93d3b4b3
Jørgen Lien Sellæg <jorgen@guut.org>
2015-05-26 16:41:50 +0200

    Remove comments

--- juci/cmake/Modules/FindLibClangmm.cmake
+++ juci/cmake/Modules/FindLibClangmm.cmake
@@ -2,10 +2,10 @@
 
 
 # Once done this will define
-#  LCL_FOUND - System has LibXml2
-#  LCL_INCLUDE_DIRS - The LibXml2 include directories
-#  LCL_LIBRARIES - The libraries needed to use LibXml2
-#  LCL_DEFINITIONS - Compiler switches required for using LibXml2
+#  LCL_FOUND - Libclangmm is available
+#  LCL_INCLUDE_DIRS - The libclangmm include directories
+#  LCL_LIBRARIES - 
+#  LCL_DEFINITIONS - Compiler switches required for using libclangmm
 
 find_package(PkgConfig)
 
@@ -21,8 +21,6 @@ set(LCL_LIBRARIES ${LCL_LIBRARY} )
 set(LCL_INCLUDE_DIRS ${LCL_INCLUDE_DIR} )
 
 include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set LIBXML2_FOUND to TRUE
-# if all listed variables are TRUE
 find_package_handle_standard_args(LCL DEFAULT_MSG
                                   LCL_LIBRARY LCL_INCLUDE_DIR)
 
