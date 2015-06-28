7ad10db1fe685a7b4ce9667d392c3b56bf39a06e
eidheim <eidheim@gmail.com>
2015-06-28 14:06:03 +0200

    Added llvm version 3.6.1.

--- juci/cmake/Modules/FindLibClang.cmake
+++ juci/cmake/Modules/FindLibClang.cmake
@@ -14,7 +14,8 @@
 #               Known LLVM release numbers.
 
 # most recent versions come first
-set(LIBCLANG_KNOWN_LLVM_VERSIONS 3.6
+set(LIBCLANG_KNOWN_LLVM_VERSIONS 3.6.1
+  3.6
   3.5.1
   3.5.0                   #Arch Linux
   3.5                     #LLVM Debian/Ubuntu packages from http://llvm.org/apt/
