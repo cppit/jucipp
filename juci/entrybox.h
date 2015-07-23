140967855ad902b264f8d468add191d518306c0b
eidheim <eidheim@gmail.com>
2015-07-23 16:10:03 +0200

    Removed old entry class that were commented out.

--- juci/entrybox.h
+++ juci/entrybox.h
@@ -25,18 +25,4 @@ public:
   std::list<Button> buttons;
 };
 
-/*class Entry: public Gtk::Box {
-public:
-  Entry();
-  void show_set_filename();
-  void show_search(const std::string& current);
-  void hide();
-  std::string operator()();
-  Gtk::Entry entry;
-  Gtk::Button button_apply_set_filename, button_close, button_next, button_prev;
-private:
-  bool on_key_press(GdkEventKey* key);
-  std::function<void()> activate;
-};*/
-
 #endif  // JUCI_ENTRYBOX_H_
