8b033f81d426afab17f637c4e68efde71c666d73
eidheim <eidheim@gmail.com>
2015-09-17 14:54:28 +0200

    More correct authors dialog.

--- src/window.cc
+++ src/window.cc
@@ -128,11 +128,10 @@ Window::Window() : box(Gtk::ORIENTATION_VERTICAL), notebook(directories), compil
   });
   
   about.set_version(Singleton::Config::window()->version);
-  about.set_authors({"Ted Johan Kristoffersen",
-                     "Jørgen Lien Sellæg",
+  about.set_authors({"Jørgen Lien Sellæg",
+                     "Ole Christian Eidheim",
                      "Geir Morten Larsen",
-                     "with special thanks to",
-                     "Ole Christian Eidheim"});
+                     "Ted Johan Kristoffersen"});
   about.set_name("About juCi++");
   about.set_program_name("juCi++");
   about.set_comments("This is an open source IDE with high-end features to make your programming experience juicy");
