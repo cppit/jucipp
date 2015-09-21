f9269690cb31cd3b315cc8000d5c706d27be7311
Jørgen Lien Sellæg <jorgen@guut.org>
2015-09-21 19:56:04 +0200

    Create enum for options

--- src/dialogs_win.cc
+++ src/dialogs_win.cc
@@ -57,22 +57,31 @@ class CommonDialog {
     DWORD options;
 };
 
+enum FILEOPENOPTIONS {
+  OVERWRITEPROMPT = 0x2, STRICTFILETYPES = 0x4, NOCHANGEDIR = 0x8, PICKFOLDERS = 0x20,
+  FORCEFILESYSTEM = 0x40, ALLNONSTORAGEITEMS = 0x80, NOVALIDATE = 0x100, ALLOWMULTISELECT = 0x200,
+  PATHMUSTEXIST = 0x800, FILEMUSTEXIST = 0x1000, CREATEPROMPT = 0x2000, SHAREAWARE = 0x4000,
+  NOREADONLYRETURN = 0x8000, NOTESTFILECREATE = 0x10000, HIDEMRUPLACES = 0x20000,
+  HIDEPINNEDPLACES = 0x40000, NODEREFERENCELINKS = 0x100000, DONTADDTORECENT = 0x2000000,
+  FORCESHOWHIDDEN = 0x10000000, DEFAULTNOMINIMODE = 0x20000000, FORCEPREVIEWPANEON = 0x40000000
+};
+
 std::string Dialog::select_folder() {
-  return (CommonDialog("Please select a folder", FOS_PICKFOLDERS)).show();
+  return (CommonDialog("Please select a folder", PICKFOLDERS)).show();
 }
 
 std::string Dialog::new_file() {
-  return (CommonDialog("Please select a folder", FOS_PICKFOLDERS)).show();
+  return (CommonDialog("Please select a folder", PICKFOLDERS)).show();
 }
 
 std::string Dialog::new_folder() {
-    return (CommonDialog("Please select a folder", FOS_PICKFOLDERS)).show();
+    return (CommonDialog("Please select a folder", PICKFOLDERS)).show();
 }
 
 std::string Dialog::select_file() {
-  return (CommonDialog("Please select a folder", FOS_PICKFOLDERS)).show();
+  return (CommonDialog("Please select a folder", PICKFOLDERS)).show();
 }
 
 std::string Dialog::save_file() {
-    return (CommonDialog("Please select a folder", FOS_PICKFOLDERS)).show();
+    return (CommonDialog("Please select a folder", PICKFOLDERS)).show();
 }
