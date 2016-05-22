f3e6116e92e2f936a044f1c2b5c1dd10b501c490
eidheim <eidheim@gmail.com>
2016-05-22 14:24:17 +0200

    Simplified setup of tests, and made tests platform independent

--- tests/stubs/terminal.cc
+++ tests/stubs/terminal.cc
@@ -6,6 +6,10 @@ bool Terminal::on_motion_notify_event(GdkEventMotion* motion_event) {return fals
 bool Terminal::on_button_press_event(GdkEventButton* button_event) {return false;}
 bool Terminal::on_key_press_event(GdkEventKey *event) {return false;}
 
+int Terminal::process(const std::string &command, const boost::filesystem::path &path, bool use_pipes) {
+  return 0;
+}
+
 size_t Terminal::print(const std::string &message, bool bold) {
   return 0;
 }
