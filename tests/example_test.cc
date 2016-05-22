3fbb9a28e40a691aa7c9cf3003c37244efed5416
eidheim <eidheim@gmail.com>
2016-05-22 15:04:27 +0200

    Cleanup of example_test comment

--- tests/example_test.cc
+++ tests/example_test.cc
@@ -4,10 +4,7 @@
 
 //In case one has to test functions that include Terminal::print or Info::print
 //Requires display server to work
-//However, it is possible to use the Broadway backend if the test is run in a pure terminal environment
-//One also has to include the source stubs/dispatcher.cc in CMakeLists.txt for at least Terminal
-
-//To run the test using broadway backend:
+//However, it is possible to use the Broadway backend if the test is run in a pure terminal environment:
 //broadwayd&
 //make test
 
