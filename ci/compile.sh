2a86c1c20b4cd2c4740b151e9488974429525c72
Jørgen Lien Sellæg <jorgen@guut.org>
2016-06-01 14:06:40 +0200

    Make sure tests displays errors on windows, fix some bad practise in bash

--- ci/compile.sh
+++ ci/compile.sh
@@ -8,6 +8,7 @@ if [ "${cmake_command}" == "" ]; then
       mingw="mingw32"
     fi
     cmake_command="cmake -G\"MSYS Makefiles\" -DCMAKE_INSTALL_PREFIX=/${mingw} -DENABLE_TESTING=1 -DCMAKE_CXX_FLAGS=-Werror .."
+    make_command="make"
   else
     cmake_command="cmake -DENABLE_TESTING=1 -DCMAKE_CXX_FLAGS=-Werror .."
   fi
@@ -17,8 +18,8 @@ if [ "${make_command}" == "" ]; then
   make_command="make -j 2"
 fi
 
-cd jucipp || (echo "Can't cd into jucipp"; return 1)
-git submodule update --init --recursive
-mkdir -p build && cd build || (echo "Error making build directory"; return 1)
-sh -c "${cmake_command}" || (echo "Cmake configuration failed"; return 1)
+cd jucipp || echo "Can't cd into jucipp"
+git submodule update --init --recursive # appveyor doesn't checkout recursively
+mkdir -p build && cd build || echo "Error making build directory"
+sh -c "${cmake_command}" || echo "Cmake configuration failed"
 exec sh -c "${make_command}"
