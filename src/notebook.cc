0406b7c1c30b9fa66feaebe03c5138255e789477
eidheim <eidheim@gmail.com>
2016-06-14 14:05:50 +0200

    Workaround for split window positioning

--- src/notebook.cc
+++ src/notebook.cc
@@ -384,9 +384,10 @@ void Notebook::toggle_split() {
     show_all();
     //Make sure the position is correct
     //TODO: report bug to gtk if it is not fixed in gtk3.22
-    //while(g_main_context_pending(NULL)) //Caused freeze on OS X sometimes
-      //g_main_context_iteration(NULL, false);
-    set_position(get_width()/2);
+    Glib::signal_timeout().connect([this] {
+      set_position(get_width()/2);
+      return false;
+    }, 50);
   }
   else {
     for(size_t c=size()-1;c!=static_cast<size_t>(-1);--c) {
