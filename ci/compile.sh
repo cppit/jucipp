9e28231191c8c23f1cf9d22e6793f5b688455069
eidheim <eidheim@gmail.com>
2016-09-20 15:43:23 +0200

    Temporarily disable error on warnings in CI's due to debian testing's gtksourceviewmm3 is not yet updated to correspond to the new gtksourceview3 package

--- ci/compile.sh
+++ ci/compile.sh
@@ -7,10 +7,10 @@ if [ "${cmake_command}" == "" ]; then
     else
       mingw="mingw32"
     fi
-    cmake_command="cmake -G\"MSYS Makefiles\" -DCMAKE_INSTALL_PREFIX=/${mingw} -DENABLE_TESTING=1 -DCMAKE_CXX_FLAGS=-Werror .."
+    cmake_command="cmake -G\"MSYS Makefiles\" -DCMAKE_INSTALL_PREFIX=/${mingw} -DENABLE_TESTING=1 .."
     make_command="make"
   else
-    cmake_command="cmake -DENABLE_TESTING=1 -DCMAKE_CXX_FLAGS=-Werror .."
+    cmake_command="cmake -DENABLE_TESTING=1 .."
   fi
 fi
 
