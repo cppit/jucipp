b2378e2039f7eccce2da1b1c6230b2ad4e247390
eidheim <eidheim@gmail.com>
2017-02-21 17:07:32 +0100

    Fixes directory/file sorting in directory view on some specific locales

--- src/directories.cc
+++ src/directories.cc
@@ -603,14 +603,14 @@ void Directories::add_or_update_path(const boost::filesystem::path &dir_path, co
       child->set_value(column_record.markup, Glib::Markup::escape_text(filename));
       child->set_value(column_record.path, it->path());
       if (boost::filesystem::is_directory(it->path())) {
-        child->set_value(column_record.id, "a"+filename);
+        child->set_value(column_record.id, '1'+filename);
         auto grandchild=tree_store->append(child->children());
         grandchild->set_value(column_record.name, std::string("(empty)"));
         grandchild->set_value(column_record.markup, Glib::Markup::escape_text("(empty)"));
         grandchild->set_value(column_record.type, PathType::UNKNOWN);
       }
       else {
-        child->set_value(column_record.id, "b"+filename);
+        child->set_value(column_record.id, '2'+filename);
         
         auto language=Source::guess_language(it->path().filename());
         if(!language)
