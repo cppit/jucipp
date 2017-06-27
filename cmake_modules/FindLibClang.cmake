d35b1fcb5cd6e24c04b7bbfd90a856fc22a5561e
eidheim <eidheim@gmail.com>
2017-06-27 22:33:01 +0200

    Added search for LLVM 4.0 on FreeBSD

--- cmake_modules/FindLibClang.cmake
+++ cmake_modules/FindLibClang.cmake
@@ -65,7 +65,7 @@ endforeach()
 find_path(LIBCLANG_INCLUDE_DIR clang-c/Index.h
   PATHS ${libclang_llvm_header_search_paths}
   PATH_SUFFIXES LLVM/include #Windows package from http://llvm.org/releases/
-    llvm39/include llvm38/include llvm37/include llvm36/include # FreeBSD
+    llvm40/include llvm39/include llvm38/include llvm37/include llvm36/include # FreeBSD
   DOC "The path to the directory that contains clang-c/Index.h")
 
 # On Windows with MSVC, the import library uses the ".imp" file extension
@@ -79,7 +79,7 @@ endif()
 find_library(LIBCLANG_LIBRARY NAMES libclang.imp libclang clang
   PATHS ${libclang_llvm_lib_search_paths}
   PATH_SUFFIXES LLVM/lib #Windows package from http://llvm.org/releases/
-    llvm39/lib llvm38/lib llvm37/lib llvm36/lib # FreeBSD
+    llvm40/lib llvm39/lib llvm38/lib llvm37/lib llvm36/lib # FreeBSD
   DOC "The file that corresponds to the libclang library.")
 
 get_filename_component(LIBCLANG_LIBRARY_DIR ${LIBCLANG_LIBRARY} PATH)
