d03ce4968c8ff2f1d6ce4c19a150cc9a3afc0af4
eidheim <eidheim@gmail.com>
2017-11-26 14:33:21 +0100

    Optimisation of Dispatcher class

--- src/dispatcher.cc
+++ src/dispatcher.cc
@@ -3,9 +3,8 @@
 Dispatcher::Dispatcher() {
   connection=dispatcher.connect([this] {
     std::unique_lock<std::mutex> lock(functions_mutex);
-    for(auto &function: functions) {
+    for(auto &function: functions)
       function();
-    }
     functions.clear();
   });
 }
@@ -14,14 +13,6 @@ Dispatcher::~Dispatcher() {
   disconnect();
   std::unique_lock<std::mutex> lock(functions_mutex);
   functions.clear();
-}
-
-void Dispatcher::post(std::function<void()> &&function) {
-  {
-    std::unique_lock<std::mutex> lock(functions_mutex);
-    functions.emplace_back(function);
-  }
-  dispatcher();
 }
 
 void Dispatcher::disconnect() {
--- src/dispatcher.h
+++ src/dispatcher.h
@@ -2,18 +2,27 @@
 #define	DISPATCHER_H_
 #include <gtkmm.h>
 #include <mutex>
-#include <vector>
+#include <list>
 
 class Dispatcher {
 private:
-  std::vector<std::function<void()>> functions;
+  std::list<std::function<void()>> functions;
   std::mutex functions_mutex;
   Glib::Dispatcher dispatcher;
   sigc::connection connection;
 public:
   Dispatcher();
   ~Dispatcher();
-  void post(std::function<void()> &&function);
+  
+  template<typename T>
+  void post(T &&function) {
+    {
+      std::unique_lock<std::mutex> lock(functions_mutex);
+      functions.emplace_back(std::forward<T>(function));
+    }
+    dispatcher();
+  }
+  
   void disconnect();
 };
 
