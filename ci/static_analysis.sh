56d711f061c661c87ddb13d80f813f542f16a787
eidheim <eidheim@gmail.com>
2017-11-28 09:33:49 +0100

    Fixed static analysis ci script. Since static analysis is performed through clang++, the clang++ compilation was removed

--- ci/static_analysis.sh
+++ ci/static_analysis.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-if [ "${make_command}" == "" ]; then
-  make_command="make -j 2"
+if [ "${cmake_command}" == "" ]; then
+  cmake_command="scan-build cmake .."
 fi
 
-cd jucipp/build || exit
-exec sh -c "scan-build -o ../html_${distribution} --status-bugs ${make_command}"
+if [ "${make_command}" == "" ]; then
+  make_command="scan-build --status-bugs make -j 2"
+fi
+
+cd jucipp || echo "Can't cd into jucipp"
+git submodule update --init --recursive # appveyor doesn't checkout recursively
+mkdir -p build && cd build || echo "Error making build directory"
+sh -c "${cmake_command}" || echo "Cmake configuration failed"
+exec sh -c "${make_command}"
